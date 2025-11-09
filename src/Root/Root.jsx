02a563970741a62c304fb0416ac25ce8fb3a6a9f
hridoy-ahmmad <hridoyp338y@gmail.com>
2025-11-09 16:10:33 +0600

    Footer

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from '../Components/Nav';
 import { Outlet } from 'react-router';
+import Footer from '../Components/Footer';
 
 const Root = () => {
     return (
         <div>
             <Nav></Nav>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
